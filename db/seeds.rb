af39b2b3c6f7dc414dd5d2f1fbeffdf921f81fcb
Adam Castro Guzman <castroguzman033@gmail.com>
2019-09-07 22:23:37 +0200

    Added the four basic logics in Alliance

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+system "clear"
+system "cls"
+
+users = ["Maryyyyyy", "Bobybob", "Cricri", "Bebert"]
+
+cpt = 1
+users.each do |user|
+  user = User.create(email: "member00#{cpt}@gmail.com", password: "123456", nickname: user, tdc: 1, need: 48)
+  cpt += 1
+end
+
+Alliance.create(name: "Les 1001 Pattes")
