be567d06632c1a39c446ddd843afabfed8cc9027
Adam Castro Guzman <castroguzman033@gmail.com>
2019-09-07 23:41:13 +0200

    done for today

---
+++ db/seed_user_data.rb
@@ -0,0 +1,52 @@
+def users
+[
+  {
+    nickname: "cricri02160",
+    tdc: 120000000000
+  },
+  {
+    nickname: "HEAT",
+    tdc: 88000000000
+  },
+  {
+    nickname: "laurent17",
+    tdc: 80000000000
+  },
+  {
+    nickname: "maryyyyyy",
+    tdc: 67000000000
+  },
+  {
+    nickname: "cricri02160",
+    tdc: 67000000000
+  },
+  {
+    nickname: "Terminator59",
+    tdc: 54000000000
+  },
+  {
+    nickname: "amentoe",
+    tdc: 48000000000
+  },
+  {
+    nickname: "Alicia974",
+    tdc: 39214056978
+  },
+  {
+    nickname: "djesx",
+    tdc: 34600000000
+  },
+  {
+    nickname: "marchombre",
+    tdc: 12771888628
+  },
+  {
+    nickname: "fenryl",
+    tdc: 7861382891
+  },
+  {
+    nickname: "Oo-laura-oO",
+    tdc: 2106463899
+  }
+]
+end
