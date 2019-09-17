9945bbcacde273cddc5dd4aa3a0f2d2cf604be62
Adam Castro Guzman <castroguzman033@gmail.com>
2019-09-17 03:09:12 +0200

    done for today

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 require_relative "seed_user_data"
+require_relative "seed_ui"
+require "colorize"
 
 system "clear"
 system "cls"
 
+puts introduction()
+
+puts "Creating users".colorize(:color => :white, :background => :red)
 cpt = 1
 users.each do |user|
   user = User.create(email: "member#{cpt}@gmail.com", password: "123456", nickname: user[:nickname], tdc: user[:tdc], need: 48)
   cpt += 1
+  puts " - #{user.nickname.green}"
 end
+
+puts "\n\n"
 
 Alliance.create(name: "Les 1001 Pattes")
